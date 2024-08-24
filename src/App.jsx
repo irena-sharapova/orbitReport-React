d464279c0cceebd85a127b1783a4c928378a14cf
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-08-23 22:36:36 -0400

    Orbit report

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Buttons from "./components/Buttons";
-import Table from "./components/Table";
+import React, { useState } from 'react';
+import satData from "./components/satData";
+import Banner from "./components/Banner.jsx";
+import Buttons from "./components/Buttons.jsx";
+import Table from "./components/Table.jsx";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const orbitTypes = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+       return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+ };
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+      filterByType={filterByType}
+      setSat={setSat}
+      orbitTypes={orbitTypes}
+      satData = {satData}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from 'react';
+import satData from "../components/satData";
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {orbitTypes.map((type, index) => (
+        <button onClick={() => filterByType(type)} key={index}>
+          {type} Orbit
+        </button>
+      ))}
+
+      <button onClick={() => setSat(satData)}>
+        All Orbits
+      </button>
     </div>
   );
 };
